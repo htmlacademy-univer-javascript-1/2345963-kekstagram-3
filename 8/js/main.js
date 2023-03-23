6ed8647f8ec60c515879c5d6183728382014b75a
Keks <keks@htmlacademy.ru>
2023-03-23 21:22:43 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import { strLenCheck } from './util.js';
+import { createPostsList } from './data.js';
+import '/js/script.js';
+
+strLenCheck('Hello, World', 15);
+createPostsList(25);
+//Задание 7 часть 2
